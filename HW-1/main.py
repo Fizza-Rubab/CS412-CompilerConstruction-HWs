a2f71ea669d87f4e25e89497260227bcd3ffa859
Iqra siddiqui <is06176@st.habib.edu.pk>
2022-09-11 17:12:26 +0500

    Update main.py

--- HW-1/main.py
+++ HW-1/main.py
@@ -1,5 +1,21 @@
-from lexical_analyzer import Lexer
+'''
+-------------------      CS-421-HOMEWORK 1-PART 2      -------------------      
+----------------            Lexical-Analyzer              --------------            
 
-lex=Lexer("test01.tpl")
+    -------------------------GROUP MEMBERS:-------------------------
+                ------Fizza Rubab
+                ------Iqra Siddiqui
+
+'''
+'''
+In this file i.e. main.py, we have imported the Lexer class from lexical_analyzer.py and activated lexical analyzer for TUPLE on test file. 
+While implementing lexical analyzer, we have assumed following:
+- %write assumptions here
+ 
+'''
+
+from lexical_analyzer import Lexer #importing Lexer class
+
+lex=Lexer("test01.tpl") 
 lex.analyze()
 lex.output()
